49a9cdd18abff618e8068a73e8b8ec8515e8640e
Dhrupa-patel <dhrupapatel337@gmail.com>
2021-11-17 19:12:37 -0700

    solved small issue

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get("/", function (res, res) {
-    response.send("Hello from Indeed backend!");
+    res.send("Hello from Indeed backend!");
 });
 
 module.exports = router;
