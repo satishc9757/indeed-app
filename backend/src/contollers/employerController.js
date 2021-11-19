642fa964786b931b94352aedb48d76d13b104d1d
Dhrupa Patel <dhrupapatel337@gmail.com>
2021-11-18 17:05:12 -0800

    making query parameter names for all API'S

--- backend/src/contollers/employerController.js
+++ backend/src/contollers/employerController.js
@@ -27,7 +27,7 @@ exports.getReviewsByCompanyIdKafka = async function (req, res) {
 };
 
 exports.getReviewsByCompanyId = async function (req, res) {
-  const compId = req.query.cmpId;
+  const compId = req.query.compId;
   try {
     let reviews = await Reviews.findAll({ where: { review_company_Id: compId } });
 
