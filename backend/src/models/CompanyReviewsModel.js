580998ff3f7e85916515164e92ad0f33812ce078
Dhrupa-patel <dhrupapatel337@gmail.com>
2021-11-21 11:59:08 -0700

    updated DB schema names

--- backend/src/models/CompanyReviewsModel.js
+++ backend/src/models/CompanyReviewsModel.js
@@ -13,10 +13,10 @@ let companyReviewModel = global.DB.define("company_reviews", {
   review_cons: { type: DataTypes.STRING, require: false },
   ceo_rating: { type: DataTypes.INTEGER, require: false },
   review_prep: { type: DataTypes.STRING, require: false },
-  found_helpul: { type: DataTypes.INTEGER, require: false },
-  found_not_helpul: { type: DataTypes.INTEGER, require: false },
-  createdAt: { type: DataTypes.DATE, require: false },
-  updatedAt: { type: DataTypes.DATE, require: false },
+  found_helpful: { type: DataTypes.INTEGER, require: false },
+  found_not_helpful: { type: DataTypes.INTEGER, require: false },
+  created_at: { type: DataTypes.DATE, require: false },
+  updated_at: { type: DataTypes.DATE, require: false },
 });
 
 module.exports = companyReviewModel;
--- backend/src/models/JobPostingsModel.js
+++ backend/src/models/JobPostingsModel.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 let jobPostingsSchema = new mongoose.Schema({
-    jobCompanyId: {type: schema.ObjectId, required: true},
-    jobTitle: {type: String, required: true},
-    jobIndustry: {type: String, required: false},
-    jobLocation: [{type: String, required: false}],
-    jobWorkType: {type: String, required: false},
-    jobSalaryDetails: {type: String, required: false},
-    jobCompensation: {type: Number, required: false},
-    jobWhatYouDo: {type: String, required: false},
-    jobWhatYouLove: {type: String, required: false},
-    jobWhatYouNeed: {type: String, required: false},
-    jobCreatedAt: {type: Date, required: false},
+    job_company_id: {type: schema.ObjectId, required: true},
+    job_title: {type: String, required: true},
+    job_industry: {type: String, required: false},
+    job_location: [{type: String, required: false}],
+    job_work_type: {type: String, required: false},
+    job_salary_details: {type: String, required: false},
+    job_compensation: {type: Number, required: false},
+    job_what_you_do: {type: String, required: false},
+    job_what_you_love: {type: String, required: false},
+    job_what_you_need: {type: String, required: false},
+    job_created_at: {type: Date, required: false},
 });
 
 const jobPostingsModel = mongoose.model('jobPostings', jobPostingsSchema);
