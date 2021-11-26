c285ad5cff371a03ba606c00808d207445742276
Satish Choudhary <satishc9757@gmail.com>
2021-11-26 01:33:21 -0800

    Frontend: Added the layout of job details card

---
+++ frontend/src/components/landingpage/JobDetailsCard.js
@@ -0,0 +1,143 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { IconButton, Link } from '@material-ui/core';
+import { Box } from '@mui/system';
+import { spacing } from '@mui/system';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+class JobDetailsCard extends Component {
+
+    state = {
+        job_company_id: "Comp1",
+        job_title: "Software Engineer Intern",
+        job_company_name: "PWC",
+        job_company_image_link: "https://uber-eats-store-0144.s3.us-east-2.amazonaws.com/images/others/pwc_bg.jpeg",
+        job_industry: "Information Technology",
+        job_location: {
+            city: "San Jose",
+            street:"7th Street",
+            state: "CA",
+            country: "USA",
+            zipcode: "95126",
+        },
+        job_work_type: "Internship",
+        job_salary_details: "$50/hour",
+        job_compensation: 50,
+        job_what_you_do: "As an Intern / Trainee, you'll work as part of a team of problem solvers, helping to solve complex business issues from strategy to execution. PwC Professional skills and responsibilities for this management level.",
+        job_what_you_love: "Our team helps multinational clients manage their mobile workforce by developing effective expatriate management solutions. You’ll be assisting our team manage business processes through expatriate software implementation, systems redesign and integration with enterprise Human Resources/Payroll solutions such as PeopleSoft, Workday, SAP and Human Resources Access.",
+        job_what_you_need: "Understanding of advanced programming concepts and object oriented design patterns, emphasizing data structures and algorithms.",
+        job_reviews: 8138,
+        jobSaved: false
+    }
+
+    handleSaveAction = () => {
+        if(!this.state.jobSaved){
+            this.setState({jobSaved: true})
+        }
+
+    }
+
+    handleUndoAction = () => {
+        if(this.state.jobSaved){
+            this.setState({jobSaved: false})
+        }
+
+    }
+
+    render() {
+
+        const favIcon = this.state.jobSaved ? <FavoriteIcon /> : <FavoriteBorderIcon />;
+        const undoButton = this.state.jobSaved ? <Button onClick={this.handleUndoAction}>Undo</Button> : null;
+
+        return (
+            <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image={this.state.job_company_image_link}
+            />
+
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                {this.state.job_title}
+                </Typography>
+                <Box display="flex" spacing={4}>
+                    <Link sx={{ml: "2rem"}}>{this.state.job_company_name}</Link>
+                    <Typography variant="body2" sx={{ml: "2rem"}} color="text.secondary">4.3</Typography>
+                    <Link sx={{m: "2rem"}}>{this.state.job_company_name}</Link>
+                </Box>
+
+                <Typography variant="body2" color="text.secondary">
+                    {this.state.job_location.city}, {this.state.job_location.state} {this.state.job_location.zipcode}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {this.state.job_work_type}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button variant="contained">Apply</Button>
+                <Button onClick={this.handleSaveAction} variant="contained" color="grey">
+                    {favIcon}
+                </Button>
+                {/* <Button startIcon={<FavoriteBorderIcon />} size="small" variant="contained"></Button> */}
+                {/* <IconButton >
+                    <FavoriteBorderIcon />
+                </IconButton> */}
+                {undoButton}
+            </CardActions>
+
+            <CardContent>
+                <Box  borderBottom={1} color="lightgrey"></Box>
+
+                    <Typography gutterBottom variant="h6" component="div">
+                        Job Details
+                    </Typography>
+                    <Typography paragraph>
+                        Salary Details: {this.state.job_salary_details}
+                    </Typography>
+
+                    <Typography paragraph>
+                        Job Type:  {this.state.job_work_type}
+                    </Typography>
+                <Box  borderBottom={1} color="lightgrey"></Box>
+
+                <Typography gutterBottom variant="h6" component="div">
+                    Full Job Description
+                </Typography>
+
+                <Typography paragraph>
+                        Job Location:  {this.state.job_location.city}, {this.state.job_location.state} {this.state.job_location.zipcode}
+                </Typography>
+                <Typography paragraph>
+                        Compensation:  {this.state.job_compensation}
+                </Typography>
+
+
+                <Typography paragraph>
+                    {this.state.job_what_you_do}
+                </Typography>
+                <Typography paragraph>
+                    {this.state.job_what_you_love}
+                </Typography>
+                <Typography paragraph>
+                    {this.state.job_what_you_need}
+                </Typography>
+
+            </CardContent>
+            </Card>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+})
+
+export default connect(mapStateToProps, {} )(JobDetailsCard)
