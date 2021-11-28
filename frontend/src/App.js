bf088620b6768720fc0d2a86f3eebc50e1bf5991
dhruvisha98 <dhruvi.m.shah@gmail.com>
2021-11-28 14:08:48 -0800

    Added login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,53 @@
-import React, { Component } from 'react'
-import './App.css'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 //redux
-import {Provider} from 'react-redux'
-import store from './redux/store'
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 //pages import
-import Home from './pages/general/home'
-import Login from './pages/user/login'
-import Signup from './pages/user/signup'
-import UploadResume from './pages/general/uploadResume'
-import Snapshot from './pages/company/snapshot'
-import Common from './pages/company/common';
-import Jobseeker from './pages/user/jobseekerProfile'
+import Home from "./pages/general/home";
+import Login from "./pages/user/Login";
+import Signup from "./pages/user/signup";
+import UploadResume from "./pages/general/uploadResume";
+import Snapshot from "./pages/company/snapshot";
+import Common from "./pages/company/common";
+import Jobseeker from "./pages/user/jobseekerProfile";
 
-import { ThemeProvider} from '@material-ui/styles'
-import { createTheme} from '@mui/material/styles'
-import { blue } from '@mui/material/colors';
-import JobDetailsCard from './components/landingpage/JobDetailsCard'
+import { ThemeProvider } from "@material-ui/styles";
+import { createTheme } from "@mui/material/styles";
+import { blue } from "@mui/material/colors";
+import JobDetailsCard from "./components/landingpage/JobDetailsCard";
 
 const theme = createTheme({
   pallete: {
-    primary:{
+    primary: {
       main: "#2557a7",
-    }
-  }
+    },
+  },
 });
 
-class App extends Component{
-
-  render(){
+class App extends Component {
+  render() {
     return (
       <Provider store={store}>
         <Router>
           <div>
             <Routes>
-              <Route exact path="/" element={<Home/>}/>
-              <Route exact path="/login" element={<Login/>} />
-              <Route exact path="/signup" element={<Signup/>} />
-              <Route path="/upload" element={<UploadResume/>} />
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/login" element={<Login />} />
               <Route exact path="/signup" element={<Signup />} />
-              <Route exact path="/common" element={<Common/>} />
-              <Route exact path="/jobseeker" element={<Jobseeker/>} />
-              <Route exact path="/jobdetails" element={<JobDetailsCard/>} />
+              <Route path="/upload" element={<UploadResume />} />
+              <Route exact path="/common" element={<Common />} />
+              <Route exact path="/jobseeker" element={<Jobseeker />} />
+              <Route exact path="/jobdetails" element={<JobDetailsCard />} />
             </Routes>
           </div>
         </Router>
       </Provider>
-    )
+    );
   }
 }
 
-export default App
+export default App;
