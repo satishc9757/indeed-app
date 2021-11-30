ada22ce21fe892492938790d29763b7717e4d20a
Dhrupa Patel <dhrupapatel337@gmail.com>
2021-11-29 19:05:39 -0800

    Update createJobApplication.js

--- kafka-backend/services/jobSeeker/createJobApplication.js
+++ kafka-backend/services/jobSeeker/createJobApplication.js
@@ -29,7 +29,7 @@ async function handle_request(msg, callback){
             app_updated_on: new Date(),
         });
 
-        application.save((err, result) => {
+        application.save(async(err, result) => {
             if(err){
                 console.error("Error in createJobApplication : " + err);
                 callback(null,{ response_code: 500, response_data: "Something went wrong!", err: err});
