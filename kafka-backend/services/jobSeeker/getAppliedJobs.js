68b1d179568aacf9230d6205133b5492ad21b8ab
Dhrupa-patel <dhrupapatel337@gmail.com>
2021-12-01 12:18:10 -0700

    added apis

---
+++ kafka-backend/services/jobSeeker/getAppliedJobs.js
@@ -0,0 +1,51 @@
+const jobSeeker = require("../../models/JobSeekersModel");
+const mongoose = require("mongoose");
+const jobPostings = require("../../models/JobPostingsModel");
+
+async function handle_request(msg, callback) {
+  const jobSeekerId = msg.jobSeekerId;
+  try {
+    const jobIds = await jobSeeker
+      .findOne({
+        seeker_id: jobSeekerId,
+      })
+      .select("seeker_job_applied");
+
+    console.log(jobIds);
+    // Array of job Objects
+    let jobs = [];
+
+    const existingJobs = await jobPostings.find();
+    //TEmp array
+
+    const jobMap = {};
+    if(existingJobs.length > 0){
+        existingJobs.forEach((job)=>{
+            jobMap[job._id] = job;
+        })
+    }
+
+    if (
+      jobIds &&
+      jobIds.seeker_job_applied &&
+      jobIds.seeker_job_applied.length > 0
+    ) {
+      jobIds.seeker_job_applied.map((jobId) => {
+        // Get details from job postings for each job Id
+        jobs.push(jobMap[jobId]);
+      });
+    }
+
+    console.log(jobs);
+    callback(null, jobs);
+  } catch (err) {
+    console.error("getJobsByCompanyId : " + err);
+    callback(null, {
+      response_code: 500,
+      response_data: "Something went wrong!",
+      err,
+    });
+  }
+}
+
+exports.handle_request = handle_request;
